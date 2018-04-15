5d3835db2bb47bec0395df392c28f13d5fd07958
Braden Pellowski <braden.w.pellowski.18@dartmouth.edu>
2018-04-15 18:26:55 -0400

    beginning# Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
-import $ from 'jquery';
-import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-//
-// var timer;
-// var timerStart;
-// var timeSpentOnSite = getTimeSpentOnSite();
-let num = 0;
-//
-// function getTimeSpentOnSite(){
-//     timeSpentOnSite = parseInt(localStorage.getItem('timeSpentOnSite'));
-//     timeSpentOnSite = isNaN(timeSpentOnSite) ? 0 : timeSpentOnSite;
-//     return timeSpentOnSite;
-// }
-//
-//
-// function startCounting(){
-//     timerStart = Date.now();
-//     timer = setInterval(function(){
-//         timeSpentOnSite = getTimeSpentOnSite()+(Date.now()-timerStart);
-//         localStorage.setItem('timeSpentOnSite',timeSpentOnSite);
-//         timerStart = parseInt(Date.now());
-//         // Convert to seconds
-//         console.log(parseInt(timeSpentOnSite/1000));
-//         num = parseInt(timeSpentOnSite/1000);
-//     },1000);
-// }
-// startCounting();
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
 
-
-function tick() {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds`);
-}
-function myFunction() {
-  setInterval(tick, 1000);
-}
-
-
-$('#main').html(`You've been on this page for ${num} seconds`);
-myFunction();
+ReactDOM.render(<App />, document.getElementById('main'));
